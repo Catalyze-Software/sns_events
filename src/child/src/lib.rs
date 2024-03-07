2b29480ef61592056c33e5db67bce647a23a12c5
Web3NL <samerweb3@gmail.com>
2024-03-07 11:57:04 +0100

    read stores

--- src/child/src/lib.rs
+++ src/child/src/lib.rs
@@ -3,6 +3,7 @@ pub static IDENTIFIER_KIND: &str = "evt";
 pub mod backup;
 pub mod default;
 pub mod methods;
+pub mod read_stores;
 mod stable_backup;
 pub mod store;
 pub mod validate;
---
+++ src/child/src/read_stores.rs
@@ -0,0 +1,23 @@
+use crate::store::ENTRIES;
+use shared::event_models::Event;
+
+#[ic_cdk::query(guard = "auth")]
+fn read_events_entries() -> Vec<(String, Event)> {
+    ENTRIES.with(|entries| entries.borrow().iter().collect::<Vec<(String, Event)>>())
+}
+
+// GUARDS
+const ALLOWED: [&str; 2] = [
+    // sam candid ui
+    "nvifv-62idm-izjcy-rvy63-7tqjz-mg2d7-jiw6m-soqvp-hdayh-mnqf5-yqe",
+    // catalyze development
+    "syzio-xu6ca-burmx-4afo2-ojpcw-e75j3-m67o5-s5bes-5vvsv-du3t4-wae",
+];
+
+fn auth() -> Result<(), String> {
+    if ALLOWED.contains(&ic_cdk::caller().to_string().as_str()) {
+        Ok(())
+    } else {
+        Err("Unauthorized".to_string())
+    }
+}
